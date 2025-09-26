5498fdb85fe71e843d1bc2e4bfd76386b0b482b4
Andrew Nathan Wong <andrew.wong@student.adelaide.edu.au>
2025-09-26 16:25:52 +0930

    (feat) : added mapbox svelte component, with functional map of Adelaide CBD displayed on web page

---
+++ frontend/src/components/Map.svelte
@@ -0,0 +1,39 @@
+<script context="module">
+    export const ssr = false;
+</script>
+
+<script>
+    import mapboxgl from 'mapbox-gl';
+    import 'mapbox-gl/dist/mapbox-gl.css';
+    import { onMount, onDestroy } from 'svelte';
+
+    let map;
+    let mapContainer;
+    const initialState = { lng: 138.599503, lat: -34.921230, zoom: 11};
+
+    onMount(() => {
+        mapboxgl.accessToken = 'pk.eyJ1IjoiYW5kcndvbmciLCJhIjoiY21nMGVoOXFmMDJoeDJqb2s3dG9oZjl3aCJ9.ksR7Z1KCEONNFmVnKX5Uaw';
+        map = new mapboxgl.Map({
+            container: mapContainer,
+            style: 'mapbox://styles/mapbox/navigation-night-v1',
+            center: [initialState.lng, initialState.lat],
+            zoom: initialState.zoom
+        });
+    });
+
+    onDestroy(() => {
+        if (map) map.remove();
+    });
+
+</script>
+
+<div class="map" bind:this={mapContainer}></div>
+
+<style>
+    .map {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        inset: 0;
+    }
+</style>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,26 +1,9 @@
-<script lang="ts">
-	import { onMount } from 'svelte';
+<script>
+	import Map from "../components/Map.svelte";
 
-	let message = 'Loading...';
-
-	onMount(async () => {
-		try {
-			const response = await fetch('/api/test');
-			const data = await response.json();
-			message = data.message;
-		} catch (error) {
-			message = 'Failed to connect to the backend.';
-			console.error(error);
-		}
-	});
 </script>
 
-<main class="flex min-h-screen flex-col items-center justify-center bg-gray-900 text-white">
-	<div class="rounded-lg bg-gray-800 p-8 text-center shadow-lg">
-		<h1 class="mb-4 text-4xl font-bold text-sky-400">FuseMap Demo</h1>
-		<p class="text-xl">Message from the backend:</p>
-		<p class="mt-2 rounded-md bg-gray-700 p-4 font-mono text-lg text-green-400">
-			{message}
-		</p>
-	</div>
+<main class="relative h-screen w-full">
+    <Map />
+    
 </main>
